5a94e80dba62aaa3da1839e2759ce39257a9d616
jradford1 <152581989+jradford1@users.noreply.github.com>
2024-06-02 13:35:39 +1000

    Final, debugged bubble chart by adding mode

--- Starter_Code/Starter_Code/static/js/app.js
+++ Starter_Code/Starter_Code/static/js/app.js
@@ -46,6 +46,7 @@ function buildCharts(sample) {
       x: otu_ids,
       y: sample_values,
       text: otu_labels,
+      mode: 'markers',
       marker: {
         size: sample_values,
         color: otu_ids,
@@ -62,7 +63,7 @@ function buildCharts(sample) {
     };
 
     // Render the Bubble Chart
-    Plotly.newPlot('bubble', bubbleChartData, bubbleChartLayout)
+    Plotly.newPlot('bubble', bubbleChartData, bubbleChartLayout);
 
     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
     let yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
@@ -93,28 +94,32 @@ function init() {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the names field
-
+    let sampleNames = data.names;
 
     // Use d3 to select the dropdown with id of `#selDataset`
-
+    let dropDown = d3.select("#selDataset");
 
     // Use the list of sample names to populate the select options
     // Hint: Inside a loop, you will need to use d3 to append a new
     // option for each sample name.
-
+    sampleNames.forEach((sample) => {
+      dropDown.append("option").text(sample).property("value", sample);
+    });
 
     // Get the first sample from the list
-
+    let firstSample = sampleNames[0];
 
     // Build charts and metadata panel with the first sample
-
+    buildCharts(firstSample)
+    buildMetadata(firstSample)
   });
 }
 
 // Function for event listener
 function optionChanged(newSample) {
   // Build charts and metadata panel each time a new sample is selected
-
+  buildCharts(newSample);
+  buildMetadata(newSample);
 }
 
 // Initialise the dashboard
